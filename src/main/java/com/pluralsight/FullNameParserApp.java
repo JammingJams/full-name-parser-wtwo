02863ac3d66455dbc7aa885d0df540acbcaaab29
JammingJams <noahlogan20o00@gmail.com>
2026-04-13 13:50:24 -0400

    Initial Commit

--- src/main/java/com/pluralsight/FullNameParserApp.java
+++ src/main/java/com/pluralsight/FullNameParserApp.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
+import java.util.InputMismatchException;
+
 public class FullNameParserApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Put your full name");
+        String fullName = sc.nextLine();
+
+        String[] tempFull = fullName.trim().split("\\s+");
+
+        int lengthFullName = tempFull.length;
+
+        if (lengthFullName > 2) {
+            System.out.println("First name: " + tempFull[0].substring(0, 1).toUpperCase() + tempFull[0].substring(1).toLowerCase() );
+            System.out.println("Middle name: " + tempFull[1].substring(0, 1).toUpperCase() + tempFull[1].substring(1).toLowerCase() );
+            System.out.println("Last name: " + tempFull[2].substring(0, 1).toUpperCase() + tempFull[2].substring(1).toLowerCase() );
+        }
+        else if (lengthFullName > 0) {
+            System.out.println("First name: " + tempFull[0].substring(0, 1).toUpperCase() + tempFull[0].substring(1).toLowerCase() );
+            System.out.println("Middle name: (none)");
+            System.out.println("Last name: " + tempFull[2].substring(0, 1).toUpperCase() + tempFull[2].substring(1).toLowerCase() );
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+
+
+    }
 }
